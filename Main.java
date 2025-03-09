821fc932eb80b0ebdd44f7578b320b2505525948
sriaaasri <psatyachowdary2001@gmail.com>
2025-03-09 15:28:47 +0530

    Forogotten to add Prime file

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Git hub project for beginners");
-    System.out.println("Application started");
+    System.out.println("Application started have a great day");
   }
 }
