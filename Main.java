cd76689efe69cfa5fb6bdea6d1ae71456a4587e7
sri_satya <91309599+sriaaasri@users.noreply.github.com>
2025-03-09 16:35:07 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,11 @@
 class Main {
+  private String name;
+  private int age;
+
+  public Main(String name , int age){
+    this.name = name;
+    this.age = age;
+  }
   public static void main(String[] args) {
     System.out.println("Git hub project for beginners");
     System.out.println("Application started have a great day");
